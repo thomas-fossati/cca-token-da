787c6dc1e0e1d900f33658ca89de23c31143bce4
Thomas Fossati <thomas.fossati@linaro.org>
2024-11-12 16:51:58 +0100

    feat: automatic build and check
    Signed-off-by: Thomas Fossati <thomas.fossati@linaro.org>

---
+++ cddl/realm/frags.mk
@@ -0,0 +1,14 @@
+REALM_FRAGS := cca-realm-claims.cddl
+REALM_FRAGS += cca-realm-challenge.cddl
+REALM_FRAGS += cca-realm-extensible-measurements.cddl
+REALM_FRAGS += cca-realm-hash-algo-id.cddl
+REALM_FRAGS += cca-realm-initial-measurement.cddl
+REALM_FRAGS += cca-realm-measurement.cddl
+REALM_FRAGS += cca-realm-mec-policy.cddl
+REALM_FRAGS += cca-realm-personalization-value.cddl
+REALM_FRAGS += cca-realm-profile.cddl
+REALM_FRAGS += cca-realm-public-key-hash-algo-id.cddl
+REALM_FRAGS += cca-realm-public-key.cddl
+REALM_FRAGS += COSE_Key.cddl
+
+REALM_EXAMPLES := $(wildcard examples/*.diag)
