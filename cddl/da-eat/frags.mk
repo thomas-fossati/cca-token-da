f9c3f1b6ae00afb35f6af8b0eee8393f15202d70
Mathieu Poirier <mathieu.poirier@linaro.org>
2025-04-10 11:07:01 -0600

    Introduction of extensions for device assignment
    This patch adds support for device attestation by extending the CCA
    attestation token with device certificate chains and measurements.  The
    layout of the certificate-log and measurement-log are tailored on the
    output of the SPDM CERTIFICATE and MEASUREMENTS messages.
    
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>

--- cddl/da-eat/frags.mk
+++ cddl/da-eat/frags.mk
@@ -1,3 +1,8 @@
 DA_EAT_FRAGS := da-eat-claims.cddl
+DA_EAT_FRAGS += claims-profile.cddl
+DA_EAT_FRAGS += certificate-log.cddl
+DA_EAT_FRAGS += measurement-log.cddl
+DA_EAT_FRAGS += measurement-block.cddl
+DA_EAT_FRAGS += measurement-blocks-signature.cddl
 
 DA_EAT_EXAMPLES := $(wildcard examples/*.diag)
