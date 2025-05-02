59342f13b9ec77adbb1646f2636d984b8db1fef3
Thomas Fossati <thomas.fossati@linaro.org>
2025-05-02 11:18:38 +0200

    slight refactoring
    Signed-off-by: Thomas Fossati <thomas.fossati@linaro.org>

--- cddl/da-token/frags.mk
+++ cddl/da-token/frags.mk
@@ -1,6 +1,7 @@
 DA_TOKEN_FRAGS := da-token.cddl
 DA_TOKEN_FRAGS += spdm-claims.cddl
 DA_TOKEN_FRAGS += spdm-measurement.cddl
+DA_TOKEN_FRAGS += spdm-certificates.cddl
 DA_TOKEN_FRAGS += spdm-measurement-blocks-signature.cddl
 DA_TOKEN_FRAGS += cxl-claims.cddl
 DA_TOKEN_FRAGS += chi-claims.cddl
